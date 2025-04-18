3a1a2f98a4b5d8ce8b2c7205ce4ef8e2491f3b62
Dome12344 <150655714+Dome12344@users.noreply.github.com>
2025-04-18 12:06:31 +0200

    Erstellung der Oberflächen von  der Lieferanten Verwaltung
    Erstellung der Auswahlen der Lieferanten Verwaltung(Lieferanten Ansicht, Lieferanten bearbeiten und Lieferanten Hinzufügen)
    Lieferanten Ansicht zu Suche eines Lieferanten
    Lieferanten bearbeiten zur bearbeiten der Informationen der Lieferanten 
    Lieferanten hinzufügen zur hinzufügen von Lieferanten der Datenbank

---
+++ Bestell- und Lagermanagement/Bestell- und Lagermanagement/Lieferanten_Verwaltung.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Bestell__und_Lagermanagement
+{
+    /// <summary>
+    /// Interaktionslogik für Lieferanten_Verwaltung.xaml
+    /// </summary>
+    public partial class Lieferanten_Verwaltung : Window
+    {
+        public Lieferanten_Verwaltung()
+        {
+            InitializeComponent();
+        }
+
+        private void lieferanten_ansicht_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void lieferanten_bearbeiten_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void lieferanten_hinzufügen_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void abmelden_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Bestell- und Lagermanagement/Bestell- und Lagermanagement/Lieferanten_hinzufügen.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Bestell__und_Lagermanagement
+{
+    /// <summary>
+    /// Interaktionslogik für Lieferanten_hinzufügen.xaml
+    /// </summary>
+    public partial class Lieferanten_hinzufügen : Window
+    {
+        public Lieferanten_hinzufügen()
+        {
+            InitializeComponent();
+        }
+
+        private void hinzufügen_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void zurück_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
