c97c39cb1a49b1feb5e044af888898b8818b7c28
Dome12344 <150655714+Dome12344@users.noreply.github.com>
2025-04-19 11:40:43 +0200

    Erstellung der Benutzeroberflächen Bestellung
    Benutzeroberflächen: Bestellung Verwaltung, Bestellung Ansicht, Bestellung bearbeiten, Bestellung Aufgeben/Stornieren erstellt

---
+++ Bestell- und Lagermanagement/Bestell- und Lagermanagement/Bestellung_Ansicht.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Bestell__und_Lagermanagement
+{
+    /// <summary>
+    /// Interaktionslogik für Bestellung_Ansicht.xaml
+    /// </summary>
+    public partial class Bestellung_Ansicht : Window
+    {
+        public Bestellung_Ansicht()
+        {
+            InitializeComponent();
+        }
+
+        private void suche_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void zurück_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Bestell- und Lagermanagement/Bestell- und Lagermanagement/Bestellung_Verwaltung.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Bestell__und_Lagermanagement
+{
+    /// <summary>
+    /// Interaktionslogik für Bestellung_Verwaltung.xaml
+    /// </summary>
+    public partial class Bestellung_Verwaltung : Window
+    {
+        public Bestellung_Verwaltung()
+        {
+            InitializeComponent();
+        }
+
+        private void bestellung_ansicht_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void bestellung_bearbeiten_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void bestellung_aufgeben_stornieren_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void abmelden_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
