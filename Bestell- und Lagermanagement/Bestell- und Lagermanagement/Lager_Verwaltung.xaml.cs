9b1ae3b3e54a59dc372c52754c92a31df7703c02
Dome12344 <150655714+Dome12344@users.noreply.github.com>
2025-04-19 13:03:47 +0200

    Erstellung der Benutzeroberfläche Lager Verwaltung
    Erstellung der Oberfläche Lager Verwaltung, Lagerinhalt Ansicht, Lager Bearbeiten, Lagerinhalt Hinzufügen

---
+++ Bestell- und Lagermanagement/Bestell- und Lagermanagement/Lager_Verwaltung.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Bestell__und_Lagermanagement
+{
+    /// <summary>
+    /// Interaktionslogik für Lager_Verwaltung.xaml
+    /// </summary>
+    public partial class Lager_Verwaltung : Window
+    {
+        public Lager_Verwaltung()
+        {
+            InitializeComponent();
+
+        }
+        private void lagerinahlt_ansicht_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void lager_bearbeiten_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void abmelden_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Bestell- und Lagermanagement/Bestell- und Lagermanagement/Lagerinhalt_Ansicht.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Bestell__und_Lagermanagement
+{
+    /// <summary>
+    /// Interaktionslogik für Lagerinhalt_Ansicht.xaml
+    /// </summary>
+    public partial class Lagerinhalt_Ansicht : Window
+    {
+        public Lagerinhalt_Ansicht()
+        {
+            InitializeComponent();
+        }
+
+        private void suche_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void zurück_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
